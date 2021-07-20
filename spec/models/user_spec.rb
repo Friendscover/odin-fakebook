0dfd7ae6413885d117c5cd4b21eaa1336043120a
Tobias Klöpper <tobiaskloepper1@web.de>
2021-07-20 10:38:14 +0200

    rubocop style fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,29 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { 
-         described_class.new(first_name: "testo", 
-                             last_name: "kesto",
-                             email: "testo@example.com",
-                             password: 123456
-         )  
-  }
+  subject do
+    described_class.new(first_name: 'testo',
+                        last_name: 'kesto',
+                        email: 'testo@example.com',
+                        password: 123_456)
+  end
 
-  context "when all params are given" do
-    it "is sucessfully created" do
+  context 'when all params are given' do
+    it 'is sucessfully created' do
       expect(subject).to be_valid
     end
   end
 
-  context "when email param is missing" do
-    it "is failing to create" do
+  context 'when email param is missing' do
+    it 'is failing to create' do
       subject.email = nil
       expect(subject).to_not be_valid
     end
   end
 
-  context "when all param are missing" do
-    it "is failing to create" do
+  context 'when all param are missing' do
+    it 'is failing to create' do
       expect(User.new).to_not be_valid
     end
   end
