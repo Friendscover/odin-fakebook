1dec7f19a4ee9bd3dfbcb45b2d22337b96c5f296
Tobias Klöpper <tobiaskloepper1@web.de>
2021-07-26 11:07:44 +0200

    updated the db seed to generate users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.all
-first = User.first.id
-second = User.second.id
+## drop table to before seeding new items
+User.delete_all
+Friendship.delete_all
 
+first_name = %w[toby ybot user tots jonas]
+last_name = %w[oby bot ser ots onas]
+email = ['toby@example.com', 'ybot@example.com', 'user@example.com', 'tots@example.com', 'jonas@example.com']
+
+# create user
+first_name.each_with_index do |_f, index|
+  User.create(first_name: first_name[index - 1],
+              last_name: last_name[index - 1],
+              email: email[index - 1],
+              password: '123456').save
+end
+
+user = User.all
+
+# create friendships to test associations
 user.each do |u|
-  u.friendships.build(friend_id: first, accepted_request: false).save
-  u.friendships.build(friend_id: second, accepted_request: true).save
+  u.friendships.build(friend_id: user.sample.id, accepted_request: false).save
+  u.friendships.build(friend_id: user.sample.id, accepted_request: true).save
 end
