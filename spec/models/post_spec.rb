81494e65fbe08f774c0dc9d130bf1b136b29d9a4
Tobias Klöpper <tobiaskloepper1@web.de>
2021-08-04 13:17:52 +0200

    added specs for the post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:userone) { User.create(first_name: 'testo', last_name: 'kesto', email: 'testo@example.com', password: 123_456) }
+
+  subject do
+    described_class.new(title: 'Hello World!',
+                        body: 'This is a test post for rspec!',
+                        user_id: userone.id)
+  end
+
+  context 'when all params are given' do
+    it 'is sucessfully created' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'when title is missing' do
+    it 'is failing to create' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'when all param are missing' do
+    it 'is failing to create' do
+      expect(User.new).to_not be_valid
+    end
+  end
+
+  context 'when user_id is missing' do
+    it 'is failing to create' do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
