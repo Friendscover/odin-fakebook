3bcfd6ba89b06965a49e4e6b02a42085e9d02787
Tobias Klöpper <tobiaskloepper1@web.de>
2021-08-10 19:12:05 +0200

    updated the db seed to generate comments, likes and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,14 @@
 ## drop table to before seeding new items
 User.delete_all
 Friendship.delete_all
+Like.delete_all
+Comment.delete_all
+Post.delete_all
 
 first_name = %w[toby ybot user tots jonas]
 last_name = %w[oby bot ser ots onas]
 email = ['toby@example.com', 'ybot@example.com', 'user@example.com', 'tots@example.com', 'jonas@example.com']
+post_title = ['Hello World!', 'Hello Ruby!', 'Hello Rails!', 'Hello Ruby on Rails!']
 
 # create user
 first_name.each_with_index do |_f, index|
@@ -23,9 +27,18 @@ first_name.each_with_index do |_f, index|
 end
 
 user = User.all
+post = Post.all
 
-# create friendships to test associations
+# create friendships, likes, posts, comments to test associations
 user.each do |u|
-  u.friendships.build(friend_id: user.sample.id, accepted_request: false).save
-  u.friendships.build(friend_id: user.sample.id, accepted_request: true).save
+  random_user = user.sample
+
+  random_user = user.sample until random_user.first_name != u.first_name
+  u.friendships.build(friend_id: random_user.id, accepted_request: false).save
+
+  u.posts.build(title: post_title.sample, body: ('a'..'z').to_a.sample(20).join).save
+  # this creates comments after a post but it should have its own loop
+  # therefore more posts are created to comment on
+  u.likes.build(user_id: user.sample.id, post_id: post.sample.id).save
+  Comment.new(commenter_id: user.sample.id, body: ('a'..'z').to_a.sample(20).join, post_id: post.sample.id).save
 end
