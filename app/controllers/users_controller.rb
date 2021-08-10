a793725f6f5bdc56bed39e429751d8daf8f953ec
Tobias Klöpper <tobiaskloepper1@web.de>
2021-08-10 17:25:54 +0200

    updated the friends request views. User can no longer accept their own requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,16 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
     @users = User.last(20)
 
-    @friend_ids = @user.friendships.map(&:friend_id)
+    @send_requests = @user.friendships
+    # { |f| f.friend.first_name }
+    # send requests equal user_id = current_user
+    @recieved_requests = Friendship.where(friend_id: current_user.id)
+    # recievedc requests equal where other users add a friend therefore they
+    # added the current_user in the friend_id column
+
+    @friends = @send_requests.map(&:friend_id)
+    @friends += @recieved_requests.map(&:user_id)
+    @friends.uniq!
 
     @posts = Post.last(10)
   end
