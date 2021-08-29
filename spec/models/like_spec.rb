5ee30c93644888cc8ed8bee14f89effd76c3fb70
Tobias Klöpper <tobiaskloepper1@web.de>
2021-08-29 21:34:15 +0200

    added a spec for the like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:userone) { User.create(first_name: 'testo', last_name: 'kesto', email: 'testo@example.com', password: 123_456) }
+  let(:usertwo) { User.create(first_name: 'two', last_name: 'two', email: 'two@example.com', password: 123_456) }
+  let(:post) { Post.create(title: 'test_post', body: 'This is a test post to test like specs!', user_id: userone.id) }
+
+  subject do
+    described_class.new(post_id: post.id,
+                        user_id: userone.id)
+  end
+
+  context 'when all params are given' do
+    it 'is sucessfully created' do
+      expect(subject).to be_valid
+    end
+  end
+
+  context 'when friend_id is missing' do
+    it 'is failing to create' do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
+  end
+
+  context 'when all param are missing' do
+    it 'is failing to create' do
+      expect(Like.new).to_not be_valid
+    end
+  end
 end
