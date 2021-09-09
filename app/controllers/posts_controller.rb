1a2f99cb9ffaf70c3f532eaef2718e8e7c965337
Tobias Klöpper <tobiaskloepper1@web.de>
2021-09-09 22:41:38 +0200

    updated the post index to show recent post of friends

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,15 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.last(10)
+    @user = User.find(current_user.id)
+
+    @friendships = Friendship.where(friend_id: current_user.id, accepted_request: true)
+    @friends = Friendship.where(user_id: current_user.id, accepted_request: true)
+
+    @friendships = @friendships.map(&:friend_id)
+    @friendships += @friends.map(&:friend_id)
+
+
+    @posts = Post.includes(:comments).where(user_id: current_user.id).or(Post.where(user_id: @friendships)).limit(10)
   end
 
   def show
